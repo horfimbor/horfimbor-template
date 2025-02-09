71563054730e213094ae3d4728901bf4105ab696
Aedius <Aedius@users.noreply.github.com>
2025-02-09 14:27:36 +0100

    upgrade engine (#12)
    refactor code to be able to listen to everything

--- server/src/consumer/dto.rs
+++ server/src/consumer/dto.rs
@@ -1,9 +1,11 @@
-use crate::{TemplateDtoCache, TemplateDtoRepository, GROUP_NAME, STREAM_NAME};
+use crate::{TemplateDtoCache, TemplateDtoRepository, STREAM_NAME};
 use anyhow::Result;
 use eventstore::Client;
 use horfimbor_eventsource::repository::Repository;
 use horfimbor_eventsource::Stream;
 use redis::Client as Redis;
+
+const GROUP_NAME: &str = "dto";
 
 pub async fn cache_dto(redis_client: Redis, event_store_db: Client) -> Result<()> {
     let dto_redis = TemplateDtoCache::new(redis_client.clone());
--- server/src/consumer/state.rs
+++ server/src/consumer/state.rs
@@ -1,9 +1,11 @@
-use crate::{TemplateRepository, TemplateStateCache, GROUP_NAME, STREAM_NAME};
+use crate::{TemplateRepository, TemplateStateCache, STREAM_NAME};
 use anyhow::Result;
 use eventstore::Client;
 use horfimbor_eventsource::repository::Repository;
 use horfimbor_eventsource::Stream;
 use redis::Client as Redis;
+
+const GROUP_NAME: &str = "state";
 
 pub async fn cache_state(redis_client: Redis, event_store_db: Client) -> Result<()> {
     let dto_redis = TemplateStateCache::new(redis_client.clone());
